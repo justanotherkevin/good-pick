5c5bb5cb2e03e0e347da5ca58ef119b4a33c8efc
Kevin Hu <hu.kevin.w@gmail.com>
2019-03-08 11:08:10 -0500

    Seperate mutation and query from component to own folder

---
+++ sick-picks/frontend/gql/query.js
@@ -0,0 +1,16 @@
+
+import gql from 'graphql-tag';
+import { perPage } from '../config';
+
+export const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
