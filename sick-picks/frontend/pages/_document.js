1ff4dc1e5b2a15dd4088d8d4037c4fab6da31fbd
Kevin Hu <hu.kevin.w@gmail.com>
2019-03-08 11:07:34 -0500

    Add comments for this file

--- sick-picks/frontend/pages/_document.js
+++ sick-picks/frontend/pages/_document.js
@@ -2,6 +2,11 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  /**
+   * * https://github.com/zeit/next.js/blob/canary/examples/with-styled-components/pages/_document.js
+   * * Commonly used to implement server side rendering for css-in-js libraries like styled-components or emotion. styled-jsx is included with Next.js by default.
+   * * getInitialProps occurs before render
+  **/
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
     const page = renderPage(App => props =>
